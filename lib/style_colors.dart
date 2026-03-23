d49133ba90ecd128c37eb49cee3f1161446da2f6
mokii543 <rivexstore@gmail.com>
2026-03-24 00:56:49 +0700

    Implementasi header biru, saldo, dan tombol

---
+++ lib/style_colors.dart
@@ -0,0 +1,18 @@
+// lib/style_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Warna biru utama khas BRImo
+  static const Color primaryBlueBrimo = Color(0xFF005b9f); 
+  // Warna biru sedikit lebih gelap untuk status bar
+  static const Color primaryDarkBlue = Color(0xFF004d8c); 
+  // Warna background abu-abu terang untuk bagian bawah kartu
+  static const Color backgroundGray = Color(0xFFF7F9FB); 
+  // Warna aksen oranye BRImo
+  static const Color accentOrange = Color(0xFFF16E3A); 
+  // Warna-warna ikon kustom
+  static const Color iconBgOrange = Color(0xFFFFECE1);
+  static const Color iconBgPink = Color(0xFFFEEDEF);
+  static const Color iconBgBlue = Color(0xFFE5F1FA);
+  static const Color iconBgTeal = Color(0xFFE0F3F1);
+}
